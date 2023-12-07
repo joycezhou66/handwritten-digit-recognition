c2bbaa82c0aedd7f30892acfc1c99fd0a30f8a21
Joyce Zhou <jezhou@usc.edu>
2023-12-07 04:25:55 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-# mnist = tf.keras.datasets.mnist
-# (x_train, y_train), (x_test, y_test) =mnist.load_data()
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) =mnist.load_data()
 
 
-# x_train = tf.keras.utils.normalize(x_train, axis=1)
-# x_test = tf.keras.utils.normalize(x_test, axis=1)
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 # model = tf.keras.models.Sequential()
 # model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
@@ -21,9 +21,9 @@ import tensorflow as tf
 
 # model.fit(x_train, y_train, epochs=3) 
 
-# model.save('handwritten.model')
+# model.save('handwritten-digit-recognition')
 
-model = tf.keras.models.load_model('handwritten.model')
+model = tf.keras.models.load_model('handwritten-digit-recognition')
 
 image_number = 1
 while os.path.isfile(f"digits/{image_number}.png"):
